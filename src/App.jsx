b0bbfe0a8706198a97e599a8b01d2d973e6aad1d
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-08 11:58:25 -0300

    understanding react state
    fixup! understanding react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { RepositoryList } from './components/RepositoryList'
+import { Contador } from './components/Contador'
 
 import './styles/global.scss'
 
 export function App() {
-  return  <RepositoryList />
+  return (
+    <> 
+      <RepositoryList />
+      <Contador />
+    </>
+  )
 }
---
+++ src/components/Contador.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export function Contador() {
+  const [counter, setCounter] = useState(0)
+
+  function increment() {
+    setCounter(counter => counter + 1)
+  }
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+
+      <button 
+        type="button" 
+        onClick={increment}
+      >
+        Increment
+      </button>
+    </div>
+  )
+}
