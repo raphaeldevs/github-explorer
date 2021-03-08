831e27a6cd6de440df676928cc0be5c5805228af
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-08 11:36:19 -0300

    First! Well... second component! 😎

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,52 @@
+const repositoryName = 'Unform'
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
