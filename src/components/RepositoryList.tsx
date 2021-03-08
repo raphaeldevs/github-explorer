806d5bd4b4a47bcc942b5d6938bdd242c8a19bdc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-08 15:19:25 -0300

    migrate from jsx to tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string
+  description: string
+  html_url: string
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -18,9 +24,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => 
-          <RepositoryItem key={repository.name} repository={repository} />)
-        }
+        {repositories.map(repository => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   )
