34182f5fa724ea57a362a4bbf57999b57e53b6c8
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-08 11:28:44 -0300

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,8 @@ module.exports = {
         use: 'babel-loader'
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ],
   }
