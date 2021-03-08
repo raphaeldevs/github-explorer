68a511f8ab99bdea5cc2f9e8addf3256048955cc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-08 12:30:17 -0300

    remove counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { RepositoryList } from './components/RepositoryList'
-import { Contador } from './components/Contador'
 
 import './styles/global.scss'
 
 export function App() {
   return (
-    <> 
-      <RepositoryList />
-      <Contador />
-    </>
+    <RepositoryList />
   )
 }
